19af6dbd5646572901329e04b5feabb63c04a377
arlo <arlottimarco17@gmail.com>
2025-03-11 20:21:19 +0000

    tested file path

---
+++ test.py
@@ -0,0 +1,20 @@
+#import tkinter as tk
+import os
+import time
+# window = tk.Tk()
+# window.title("AUTOMATIC_XEPATCHER")
+
+
+def start_patching(): #the program starts to patching files
+    os.system("cls")
+    os.system("cd .XePatcher")
+    # os.system("XexTool.exe -m r -r a automatic_XePatcher\input\\test.xex")
+# XexTool.exe -m r -r a C:\Users\lenovo\OneDrive\Desktop\PROGRAMMI\repositories\automatic_XePatcher\input\test.xex
+
+start_patching()
+# button = tk.Button(text="START PATCHING")
+# button.bind("test", start_patching)
+# button.pack()
+
+# Start the event loop.
+# window.mainloop()
